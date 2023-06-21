11d42fd613b95a80865b4d95c29565c626d28a75
Chloé <114312337+Chloe-2622@users.noreply.github.com>
2023-06-21 16:07:34 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+});
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  myHeading.textContent = 'Mozilla est cool, ' + myName;
+}
+
+myButton.addEventListener('click', function() {
+  setUserName();
+});
